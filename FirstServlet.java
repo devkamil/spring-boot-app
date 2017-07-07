c22d41d77c0b61ecbaaa8d008ed4eed9fb26505b
devkamil <kamilkarwowski.dev@gmail.com>
2017-07-07 16:24:46 +0200

    FirstServlet v.0.2

--- FirstServlet.java
+++ FirstServlet.java
@@ -2,6 +2,9 @@ package pl.devkamil;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,18 +32,29 @@ public class FirstServlet extends HttpServlet {
 //        // TODO Auto-generated constructor stub
 //    }
 
+	
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:ss");
+	Date date = new Date();
+	
     
     protected void firstServlet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	response.setContentType("text/html;charset=UTF-8");
     	PrintWriter writer = response.getWriter();
     	try{
-    		String text = request.getParameter("emptyText");
+    		String topic = request.getParameter("topic");
+    		String content = request.getParameter("content");
+    		String author = request.getParameter("author");
+    		String currentDate = dateFormat.format(date);
+    		
     		writer.println("<html>");
     		writer.println("<head>");
     		writer.println("<title>My First Servlet-Servlet-Servlet!</title>");
     		writer.println("</head>");
     		writer.println("<body>");
-    		writer.println("<h1>This is my first servlet! " + text + ", " + text +  "</h1>");
+    		writer.println("<h1>Temat: " + topic +  "</h1>");
+    		writer.println("<h1>Treść: " + content +  "</h1>");
+    		writer.println("<h1>Autor: " + author +  "</h1>");
+    		writer.println("<h1>Data: " + currentDate +  "</h1>");
     		writer.println("</body>");
     		writer.println("</html>");
     		
--- index.jsp
+++ index.jsp
@@ -1,17 +1,22 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>First Servlet</title>
 </head>
 <body>
 	
 	<h1>My First Servlet!</h1>
 	<form action="FirstServlet.do">
-		<p>Podaj tekst:</p>
-		<input type="text" name="emptyText" /><br>
+		<p>Temat:</p>
+		<input type="text" name="topic" />
+		<p>Treść notatki:</p>
+		<textarea rows="4" cols="50" name="content">
+		Treść notatki...</textarea>
+		<p>Autor:</p>
+		<input type="text" name="author" /><br /><br />
 		<input type="submit" value="OK" />
 	</form>
 </body>
